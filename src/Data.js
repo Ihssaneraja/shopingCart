2ff027b7224fa998a4bcaa7605b04f2a156af40a
Ihssaneraja <contactihssane@gamil.comgit config --global Ihssaneraja ihssaneraja>
2023-09-29 05:45:29 +0200

    localstorage shoping cart js

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Casual basket",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/basket.webp",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Office basket",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/basket-sport.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "sport basket",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/mens.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Mens basket",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/basket.webp",
+  },
+];
